121756b13fdf49d0058c44e0231795c37535b5f3
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:41:24 -0600

    Rewriting as Laminas Project package

--- src/Adapter/IgBinary.php
+++ src/Adapter/IgBinary.php
@@ -1,16 +1,15 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-serializer for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-serializer/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-serializer/blob/master/LICENSE.md New BSD License
  */
 
-namespace Zend\Serializer\Adapter;
+namespace Laminas\Serializer\Adapter;
 
-use Zend\Serializer\Exception;
-use Zend\Stdlib\ErrorHandler;
+use Laminas\Serializer\Exception;
+use Laminas\Stdlib\ErrorHandler;
 
 class IgBinary extends AbstractAdapter
 {
