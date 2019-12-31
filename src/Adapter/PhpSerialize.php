a5067ab7db345efa9f7157a4a22496293e115e93
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:41:15 -0600

    Rewriting as Laminas Project package

--- src/Adapter/PhpSerialize.php
+++ src/Adapter/PhpSerialize.php
@@ -1,21 +1,19 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- * @package   Zend_Serializer
+ * @see       https://github.com/laminas/laminas-serializer for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-serializer/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-serializer/blob/master/LICENSE.md New BSD License
  */
 
-namespace Zend\Serializer\Adapter;
+namespace Laminas\Serializer\Adapter;
 
-use Zend\Serializer\Exception;
-use Zend\Stdlib\ErrorHandler;
+use Laminas\Serializer\Exception;
+use Laminas\Stdlib\ErrorHandler;
 
 /**
- * @category   Zend
- * @package    Zend_Serializer
+ * @category   Laminas
+ * @package    Laminas_Serializer
  * @subpackage Adapter
  */
 class PhpSerialize extends AbstractAdapter
