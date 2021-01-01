2b5d440e6ff08c2cb4eebe77eb7152752a3a2f11
Marc-Jan Barnhoorn <marc-jan@gekvansoftware.nl>
2021-01-01 12:09:06 +0100

    added support for 'serializers' from config
    Signed-off-by: Marc-Jan Barnhoorn <marc-jan@gekvansoftware.nl>

--- src/AdapterPluginManagerFactory.php
+++ src/AdapterPluginManagerFactory.php
@@ -9,6 +9,7 @@
 namespace Laminas\Serializer;
 
 use Interop\Container\ContainerInterface;
+use Laminas\ServiceManager\Config;
 use Laminas\ServiceManager\FactoryInterface;
 use Laminas\ServiceManager\ServiceLocatorInterface;
 
@@ -28,7 +29,30 @@ class AdapterPluginManagerFactory implements FactoryInterface
      */
     public function __invoke(ContainerInterface $container, $name, array $options = null)
     {
-        return new AdapterPluginManager($container, $options ?: []);
+        $pluginManager = new AdapterPluginManager($container, $options ?: []);
+
+        // If this is in a laminas-mvc application, the ServiceListener will inject
+        // merged configuration during bootstrap.
+        if ($container->has('ServiceListener')) {
+            return $pluginManager;
+        }
+
+        // If we do not have a config service, nothing more to do
+        if (! $container->has('config')) {
+            return $pluginManager;
+        }
+
+        $config = $container->get('config');
+
+        // If we do not have serializers configuration, nothing more to do
+        if (! isset($config['serializers']) || ! is_array($config['serializers'])) {
+            return $pluginManager;
+        }
+
+        // Wire service configuration for serializers
+        (new Config($config['serializers']))->configureServiceManager($pluginManager);
+
+        return $pluginManager;
     }
 
     /**
