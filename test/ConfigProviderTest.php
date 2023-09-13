0e4a4df04febc43f20dd73b272cfa0a17f2c79a1
Maximilian Bösing <2189546+boesing@users.noreply.github.com>
2023-09-13 23:40:10 +0200

    qa: add unit test to verify services provided by the `ConfigProvider`
    Signed-off-by: Maximilian Bösing <2189546+boesing@users.noreply.github.com>

--- test/ConfigProviderTest.php
+++ test/ConfigProviderTest.php
@@ -5,23 +5,31 @@ declare(strict_types=1);
 namespace LaminasTest\Serializer;
 
 use Laminas\Serializer\Adapter\AdapterInterface;
+use Laminas\Serializer\AdapterPluginManager;
 use Laminas\Serializer\ConfigProvider;
 use PHPUnit\Framework\TestCase;
 
 final class ConfigProviderTest extends TestCase
 {
-    private ConfigProvider $provider;
-
-    protected function setUp(): void
+    /**
+     * @param non-empty-string $serviceName
+     * @dataProvider factoryServiceNames
+     */
+    public function testHasExpectedServiceNames(string $serviceName): void
     {
-        parent::setUp();
-        $this->provider = new ConfigProvider();
+        $dependencies = (new ConfigProvider())->getDependencyConfig();
+        self::assertArrayHasKey($serviceName, $dependencies['factories']);
     }
 
-    public function testWillProvideDefaultSerializer(): void
+    /**
+     * @return iterable<non-empty-string,array{0:non-empty-string}>
+     */
+    public static function factoryServiceNames(): iterable
     {
-        $dependencies = $this->provider->getDependencyConfig();
-        $factories    = $dependencies['factories'] ?? [];
-        self::assertArrayHasKey(AdapterInterface::class, $factories);
+        return [
+            'SerializerAdapterManager'  => ['SerializerAdapterManager'],
+            AdapterPluginManager::class => [AdapterPluginManager::class],
+            AdapterInterface::class     => [AdapterInterface::class],
+        ];
     }
 }
